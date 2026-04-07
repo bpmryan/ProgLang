535d03568a8bd6e201b4fa65f061510bf061dfc7
bpmryan <bpm_ryand2428@gmail.com>
2026-04-07 15:31:52 -0500

    Add global variables and function calls

--- Homework05_Dinh.java
+++ Homework05_Dinh.java
@@ -1,5 +1,42 @@
+import java.io.*;
+
 public class Homework05_Dinh {
+
+    // global variables
+    static int charClass;
+    static char lexeme[100];
+    static int nextChar;
+    static int token;
+    static int nextToken;
     public static void main(String[] args) {
+        // function declarations
+        addChar();
+        getChar();
+        getNonBlank();
+        lex();
+    }
+
+    public void LexicalAnalyzer() {
         
+    }
+
+    private static void lex() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'lex'");
+    }
+
+    private static void getChar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getChar'");
+    }
+
+    private static void getNonBlank() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNonBlank'");
+    }
+
+    private static void addChar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addChar'");
     }
 }
